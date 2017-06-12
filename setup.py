a9b33d29dbdb09399938e1d144dc956a2a90be8d
hagne <hagen@hagne.net>
2017-06-12 15:38:10 -0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='erainterim',
+    version='',
+    packages=['erainterim'],
+    url='',
+    license='',
+    author='Hagen Telg',
+    author_email='hagen@hagnet.net',
+    description='',
+    install_requires = ['numpy', 'pandas', 'matplotlib', 'mpl_toolkits', 'netCDF4', 'xarray'],
+    # extra_requires=['newrelic'],
+)
