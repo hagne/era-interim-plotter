60e198c25e47831091a1bb09ea5fd8d96f420f1e
hagne <hagen@hagne.net>
2017-06-16 11:45:44 -0600

    its somewhat working now

--- erainterim/era_lab.py
+++ erainterim/era_lab.py
@@ -1,6 +1,6 @@
 import xarray as _xr
-import matplotlib as _plt
-from mpl_toolkits import Basemap
+import matplotlib.pylab as _plt
+from mpl_toolkits.basemap import Basemap as _Basemap
 from mpl_toolkits.axes_grid1 import make_axes_locatable as _make_axes_locatable
 import matplotlib.colors as _colors
 import numpy as _np
@@ -13,6 +13,20 @@ import pandas as _pd
 #                       {'name_internal':'wind_speed10',
 #                        'name_nc_variable': None}]
 
+
+def load_nc(fname):
+    #     nc = Dataset(fname)
+    xr_set = _xr.open_dataset(fname)
+    era_int = EraInterim()
+    for var in xr_set.variables.keys():
+        if var in ['longitude', 'latitude', 'time']:
+            setattr(era_int, var, xr_set.variables[var])
+        else:
+            setattr(era_int, "_" + var, xr_set.variables[var])
+
+    era_int._generate_properties()
+    return era_int
+
 class EraInterim(object):
     def _generate_properties(self):
         if hasattr(self, '_v10') and hasattr(self, '_u10'):
@@ -20,25 +34,24 @@ class EraInterim(object):
 
 
 class AtmProperty(object):
-    def plot_map(self, extend=1000000, lat_0=42.030484, lon_0=-70.049392):
-        f, a = _plt.subplots()
-        bmap = Basemap(projection='aeqd',
-                       lat_0=lat_0,
-                       lon_0=lon_0,
-                       width=extend,
-                       height=extend,
-                       resolution='i',
-                       ax=a)
+    def plot_map(self, extend=1000000, lat_0=42.030484, lon_0=-70.049392, ax = None):
+        if ax:
+            a = ax
+            f = a.get_figure()
+        else:
+            f, a = _plt.subplots()
+        bmap = _Basemap(projection='aeqd',
+                        lat_0=lat_0,
+                        lon_0=lon_0,
+                        width=extend,
+                        height=extend,
+                        resolution='i',
+                        ax=a)
         # bmap.shadedrelief()
         bmap.bluemarble()
 
         bmap.drawcoastlines(zorder=100)
         return f, a, bmap
-
-
-# nop = 40
-#         lons, lats, x, y = bmap.makegrid(nop, nop, returnxy=True)
-#         X,Y = np.meshgrid(v10psel.columns,v10psel.index)
 
 class Wind(AtmProperty):
     def __init__(self, era_int, v, u):
@@ -54,8 +67,29 @@ class Wind(AtmProperty):
             self._wind_speed = _np.sqrt(self.u ** 2 + self.v ** 2)
         return self._wind_speed
 
-    def plot_streamlines(self, time2use_idx):
-        f, a, bmap = super().plot_map()
+
+    def plot_streamlines(self, time = 'all', vmin = None, vmax = None, ax = None):
+        """
+        Parameters
+        ----------
+        time: string, int, or Timestamp (Timestamp not working right now)
+            When more then one dataset is included you can select a particular by eigher giving the the index (int) or
+            the timestamp (pandas Timestamp or string). Or you can choose to plot all in separate subplots.
+        """
+        if type(time) == int:
+            f, a, sl, cb = self._plot_streamlines(time, vmin = vmin, vmax = vmax, ax = ax)
+        elif time == 'all':
+            if type(ax) != type(None):
+                raise ValueError('Ax has to be None if multiple plots are beeing plotted. Select a particular plot or set ax to None.')
+            f,a = _plt.subplots(1,len(self._era_int.time))
+            for e, at in enumerate(a):
+                f, at, sl, cb = self._plot_streamlines(e, ax = at, vmin = vmin, vmax = vmax)
+
+        return f,a,sl,cb
+
+
+    def _plot_streamlines(self, time2use_idx, ax = None, vmin = None, vmax = None):
+        f, a, bmap = super().plot_map(ax = ax)
 
         # generate evenly spaced grid
         nop = 40
@@ -78,10 +112,8 @@ class Wind(AtmProperty):
         speed = _np.sqrt(new_u10psel ** 2 + new_v10psel ** 2)
 
         # plot streamlines
-        # cmap = plt.cm.YlGnBu
         cmap = _plt.cm.YlOrBr
-        vmin = 0
-        vmax = 13
+
         lwmax = 3
         lwmin = 0.2
         lw = (speed * (lwmax - lwmin) / (vmax - vmin))
@@ -92,7 +124,7 @@ class Wind(AtmProperty):
                           density=3,
                           color=speed,
                           cmap=cmap,
-                          norm=_colors.Normalize(vmin=0, vmax=20),
+                          norm=_colors.Normalize(vmin=vmin, vmax=vmax),
                           linewidth=lw  # 0.5*speed
                           )
         # a.pcolormesh(x,y,new_z)
@@ -104,50 +136,4 @@ class Wind(AtmProperty):
 
         time2use = _pd.Timestamp(self._era_int.time.values[time2use_idx])
         a.set_title(time2use, loc='left')
-        #         return X
         return f, a, sl, cb
-
-
-def load_nc(fname):
-    #     nc = Dataset(fname)
-    xr_set = _xr.open_dataset(fname)
-    era_int = EraInterim()
-    for var in xr_set.variables.keys():
-        if var in ['longitude', 'latitude', 'time']:
-            setattr(era_int, var, xr_set.variables[var])
-        else:
-            setattr(era_int, "_" + var, xr_set.variables[var])
-
-    era_int._generate_properties()
-    return era_int
-    #     lon = nc.variables['longitude'][:]
-    #     era_int.lon = np.mod(lon[:]+180,360) - 180
-    #     era_int.lat = nc.variables['latitude'][:]
-    #     time = nc.variables['time'][:]
-    #     era_int.time_index = pd.to_datetime(num2date(time[:],'hours since 1900-01-01 00:00:0.0'))
-    #     for var in nc.variables.keys():
-    #         if var in ['longitude', 'latitude', 'time']:
-    #             continue
-    #         print(var)
-    #         res = [d for d in era_interim_params if d.get('name_nc_variable', '') == var]
-    #         if len(res) == 0:
-    #             raise KeyError('Variable "{}" has not been defined in era_interim_params ... do so!'.format(var))
-    #         else:
-    #             res = res[0]
-    #         print(res)
-    #         var_dat = nc.variables[var][:]
-    #         var_df = pd.Panel(var_dat[:], items=era_int.time_index, major_axis= era_int.lat, minor_axis=era_int.lon)
-
-    #         var_df.items.name = 'Time'
-    #         var_df.major_axis.name = 'Lat'
-    #         var_df.minor_axis.name = 'Lon'
-    #         var_df = var_df.sort_index(2)
-
-    # #         break
-    #     #     setattr(era_int, res['name_internal'], var_df)
-    #         setattr(era_int, '_' + var, var_df)
-    #     era_int.lon.sort()
-
-    #
-    #     return var_dat, era_int.time_index, era_int.lat, era_int.lon#era_int
-    #     return era_int
